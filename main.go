8c38f2313c125646a8a05c43826dac2213f88827
Agung <Prabowoa63@gmail.com>
2022-03-04 22:57:38 +0700

    Adding env variable

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import "hb-backend-v1/routes"
 import "hb-backend-v1/config/database"
-import _"fmt"
+import "fmt"
+import "github.com/joho/godotenv"
 
 
 func main(){
 	router := routes.Routes()
+	err := godotenv.Load()
+	if err!=nil{
+		fmt.Println("Failed to load .env file")
+	}
 	database.InitDB()
 	router.Run(":3001")
 }
