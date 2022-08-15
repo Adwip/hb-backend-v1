64ac8273c564f02f727e74e1075c05e88b4705b6
Agung <Prabowoa63@gmail.com>
2022-08-15 14:50:31 +0700

    Add product recomendation

--- service/product.go
+++ service/product.go
@@ -7,6 +7,7 @@ import "hb-backend-v1/model"
 type Product interface {
 	AddProduct(*gin.Context, *model.AddProductRequest) (bool, string, string)
 	AddProductImages(*gin.Context, *[]model.ProductImage, string) (bool, []string)
+	ProductRecommended(*gin.Context) []model.AllProductsResponse
 	// UpdateProduct()
 	// DeleteProduct()
 }
@@ -31,4 +32,8 @@ func (service ProductService) AddProduct(c *gin.Context, req *model.AddProductRe
 func (service ProductService) AddProductImages(c *gin.Context, req *[]model.ProductImage, productID string) (bool, []string) {
 
 	return service.productImageRepo.AddProductImages(c, productID, *req)
+}
+
+func (service ProductService) ProductRecommended(c *gin.Context) []model.AllProductsResponse {
+	return service.productRepo.ProductRecommended(c)
 }
